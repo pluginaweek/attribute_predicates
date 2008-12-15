f75a18fcb2d95884019706be1beab7acecd7f7a6
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2008-12-14 20:38:17 -0500

    Remove the PluginAWeek namespace

--- lib/attribute_predicates/extensions/active_record.rb
+++ lib/attribute_predicates/extensions/active_record.rb
@@ -1,53 +1,51 @@
-module PluginAWeek #:nodoc:
-  module AttributePredicates
-    module Extensions
-      # Adds support for automatically defining predicate methods using +attr_predicate+
-      # when defining attributes using +attr+, +attr_reader+, +attr_reader+, and
-      # +attr_accessor+.  In comparison to normal Ruby attributes, ActiveRecord
-      # predicates use a different system for defining true/false.
-      # 
-      # == Examples
-      # 
-      # The predicate methods for attributes use ActiveRecord's type conversion
-      # for booleans for determing whether to return true or false.  For example,
-      # 
-      #   class Person < ActiveRecord::Base
-      #     attr_accessor :value
-      #   end
-      #   
-      #   p = Person.new
-      #   p.value = false
-      #   p.value?    # => false
-      #   
-      #   p.value = 'false'
-      #   p.value?    # => false
-      #   
-      #   p.value = 'true'
-      #   p.value?    # => true
-      #   
-      #   p.value = 't'
-      #   p.value?    # => true
-      #   
-      #   p.value = 1
-      #   p.value?    # => true
-      module ActiveRecord
-        private
-          # For Strings, returns true when value is:
-          # * "true"
-          # * "t"
-          # 
-          # For Integers, returns true when value is:
-          # * 1
-          def attr_predicate(symbol)
-            define_method("#{symbol}?") do
-              ::ActiveRecord::ConnectionAdapters::Column.value_to_boolean(instance_variable_get("@#{symbol}")) == true
-            end
+module AttributePredicates
+  module Extensions
+    # Adds support for automatically defining predicate methods using +attr_predicate+
+    # when defining attributes using +attr+, +attr_reader+, +attr_reader+, and
+    # +attr_accessor+.  In comparison to normal Ruby attributes, ActiveRecord
+    # predicates use a different system for defining true/false.
+    # 
+    # == Examples
+    # 
+    # The predicate methods for attributes use ActiveRecord's type conversion
+    # for booleans for determing whether to return true or false.  For example,
+    # 
+    #   class Person < ActiveRecord::Base
+    #     attr_accessor :value
+    #   end
+    #   
+    #   p = Person.new
+    #   p.value = false
+    #   p.value?    # => false
+    #   
+    #   p.value = 'false'
+    #   p.value?    # => false
+    #   
+    #   p.value = 'true'
+    #   p.value?    # => true
+    #   
+    #   p.value = 't'
+    #   p.value?    # => true
+    #   
+    #   p.value = 1
+    #   p.value?    # => true
+    module ActiveRecord
+      private
+        # For Strings, returns true when value is:
+        # * "true"
+        # * "t"
+        # 
+        # For Integers, returns true when value is:
+        # * 1
+        def attr_predicate(symbol)
+          define_method("#{symbol}?") do
+            ::ActiveRecord::ConnectionAdapters::Column.value_to_boolean(instance_variable_get("@#{symbol}")) == true
           end
-      end
+        end
     end
   end
 end
 
 ActiveRecord::Base.class_eval do
-  extend PluginAWeek::AttributePredicates::Extensions::ActiveRecord
+  extend AttributePredicates::Extensions::ActiveRecord
 end if defined?(ActiveRecord)
--- lib/attribute_predicates/extensions/module.rb
+++ lib/attribute_predicates/extensions/module.rb
@@ -1,81 +1,79 @@
-module PluginAWeek #:nodoc:
-  module AttributePredicates
-    module Extensions
-      # Adds support for automatically defining predicate methods using +attr_predicate+
-      # when defining attributes using +attr+, +attr_reader+, +attr_reader+, and
-      # +attr_accessor+.
+module AttributePredicates
+  module Extensions
+    # Adds support for automatically defining predicate methods using +attr_predicate+
+    # when defining attributes using +attr+, +attr_reader+, +attr_reader+, and
+    # +attr_accessor+.
+    # 
+    # == Examples
+    # 
+    # The predicate methods for attributes checks whether the value is blank?
+    # for determining whether true or false should be returned.  For example,
+    # 
+    #   class Person
+    #     attr_accessor :value
+    #   end
+    #   
+    #   p = Person.new
+    #   p.value = false
+    #   p.value?    # => false
+    #   
+    #   p.value = true
+    #   p.value?    # => true
+    #   
+    #   p.value = []
+    #   p.value?    # => false
+    #   
+    #   p.value = [false]
+    #   p.value?    # => true
+    module Module
+      def self.included(base) #:nodoc:
+        base.class_eval do
+          %w(attr attr_reader attr_writer attr_accessor).each do |method|
+            alias_method "#{method}_without_predicates", method
+            alias_method method, "#{method}_with_predicates"
+          end
+        end
+      end
+      
+      # Defines a predicate method, using +attr_predicate+, in addition to the
+      # attribute accessors.  For example,
       # 
-      # == Examples
-      # 
-      # The predicate methods for attributes checks whether the value is blank?
-      # for determining whether true or false should be returned.  For example,
-      # 
-      #   class Person
-      #     attr_accessor :value
+      #   module Mod
+      #     attr :is_okay
       #   end
       #   
-      #   p = Person.new
-      #   p.value = false
-      #   p.value?    # => false
-      #   
-      #   p.value = true
-      #   p.value?    # => true
-      #   
-      #   p.value = []
-      #   p.value?    # => false
-      #   
-      #   p.value = [false]
-      #   p.value?    # => true
-      module Module
-        def self.included(base) #:nodoc:
-          base.class_eval do
-            %w(attr attr_reader attr_writer attr_accessor).each do |method|
-              alias_method "#{method}_without_predicates", method
-              alias_method method, "#{method}_with_predicates"
-            end
-          end
-        end
-        
-        # Defines a predicate method, using +attr_predicate+, in addition to the
-        # attribute accessors.  For example,
-        # 
-        #   module Mod
-        #     attr :is_okay
-        #   end
-        #   
-        #   Mod.instance-methods.sort # => ["is_okay", "is_okay?"]
-        def attr_with_predicates(*args)
-          attr_without_predicates(*args)
-          attr_predicate(args.first)
-        end
-        
-        [:attr_reader, :attr_writer, :attr_accessor].each do |method|
-          define_method("#{method}_with_predicates") do |*symbols|
-            send("#{method}_without_predicates", *symbols)
-            symbols.each {|symbol| attr_predicate(symbol)}
-          end
-        end
-        
-        private
-          # Returns true if the specified variable is not blank, otherwise false
-          def attr_predicate(symbol)
-            define_method("#{symbol}?") do
-              value = instance_variable_get("@#{symbol}")
-              if value.respond_to?(:blank?)
-                # Use ActiveSupport's implementation
-                !value.blank?
-              elsif value.respond_to?(:empty?)
-                !value.empty?
-              else
-                !!value
-              end
-            end
-          end
+      #   Mod.instance-methods.sort # => ["is_okay", "is_okay?"]
+      def attr_with_predicates(*args)
+        attr_without_predicates(*args)
+        attr_predicate(args.first)
       end
+      
+      [:attr_reader, :attr_writer, :attr_accessor].each do |method|
+        define_method("#{method}_with_predicates") do |*symbols|
+          send("#{method}_without_predicates", *symbols)
+          symbols.each {|symbol| attr_predicate(symbol)}
+        end
+      end
+      
+      private
+        # Returns true if the specified variable is not blank, otherwise false
+        def attr_predicate(symbol)
+          define_method("#{symbol}?") do
+            value = instance_variable_get("@#{symbol}")
+            if value.respond_to?(:blank?)
+              # Use ActiveSupport's implementation
+              !value.blank?
+            elsif value.respond_to?(:empty?)
+              !value.empty?
+            else
+              !!value
+            end
+          end
+        end
     end
   end
 end
 
 ::Module.class_eval do
-  include PluginAWeek::AttributePredicates::Extensions::Module
+  include AttributePredicates::Extensions::Module
 end
